6a13d12be5d7fc25bcd1efc163a138c52f01748a
iqbalapipahnoer <gomaniqbal@student.telkomuniversity.ac.id>
2023-06-04 22:31:37 +0700

    Menambahkan Materi Video

--- app/src/main/java/org/d3ifcool3046/pt2/KotakActivity.kt
+++ app/src/main/java/org/d3ifcool3046/pt2/KotakActivity.kt
@@ -2,10 +2,30 @@ package org.d3ifcool3046.pt2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
 
 class KotakActivity : AppCompatActivity() {
+    private lateinit var youtubePlayerView: YouTubePlayerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotak)
-        }
+        youtubePlayerView = findViewById(R.id.youtubePlayerViewKotak)
+
+        lifecycle.addObserver(youtubePlayerView)
+
+        youtubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                youTubePlayer.cueVideo("ScR8h2hIuMQ", 0f)
+                youTubePlayer.pause()
+            }
+        })
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        youtubePlayerView.release()
+    }
+}
