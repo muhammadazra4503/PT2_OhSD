5885a06a954174e38611521b6fbebd79d31b2bf3
azra <91065112+muhammadazra4503@users.noreply.github.com>
2023-06-10 15:59:33 +0700

    Add Medium and Hard Quiz

--- app/src/main/java/org/d3ifcool3046/pt2/Util.kt
+++ app/src/main/java/org/d3ifcool3046/pt2/Util.kt
@@ -8,9 +8,11 @@ object Util {
     const val NAME: String = "name"
     const val TOTAL_PERTANYAAN: String = "total_pertanyaan"
     const val JAWABAN_BENAR: String = "jawaban_benar"
+    private val quizList = ArrayList<Question>()
+    private val quizList2 = ArrayList<Question>()
+    private val quizList3 = ArrayList<Question>()
 
     fun setEasyQuestion(): ArrayList<Question> {
-        val quizList = ArrayList<Question>()
         val question1 = Question(
             "Sebuah segitiga memiliki ukuran sisi alas 10 cm dan tinggi 7 cm. Jika ukuran sisi-sisi miringnya adalah 8 cm dan 9 cm, hitunglah berapa luas dan keliling segitiga tersebut!",
             "35 cm² dan 27 cm",
@@ -96,85 +98,84 @@ object Util {
     }
 
     fun setMediumQuestion(): ArrayList<Question> {
-        val quizList2 = ArrayList<Question>()
         val question1 = Question(
-            "2",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            1)
+            "Sepetak sawah berbentuk persegi dengan panjang sisi 50 m. Luas sawah tersebut adalah .... cm²",
+            "2.000cm²",
+            "2.250cm²",
+            "2.500cm²",
+            3)
         quizList2.add(question1)
 
         val question2 = Question(
-            "5",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
+            "Keliling suatu bangun persegi 60 cm. Luas bangun tersebut adalah .... cm²",
+            "220cm²",
+            "225cm²",
+            "235cm²",
             2)
         quizList2.add(question2)
 
         val question3 = Question(
-            "3",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            2)
+            "Jika panjang sisi persegi 48 cm, maka kelilingnya adalah .... cm",
+            "182cm",
+            "172cm",
+            "192cm",
+            3)
         quizList2.add(question3)
 
         val question4 = Question(
-            "4",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            2)
+            "Diketahui 1/2 x p x l = 120 cm². Jika l = 20 cm, maka nilai p adalah .... cm",
+            "12cm",
+            "14cm",
+            "16cm",
+            1)
         quizList2.add(question4)
 
         val question5 = Question(
-            "5",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
+            "Sebuah papan reklame berbentuk segitiga, memiliki panjang alas 65 cm dan luasnya 1.625 cm², maka tingginya adalah .... cm",
+            "52cm",
+            "50cm",
+            "55cm",
             2)
         quizList2.add(question5)
 
         val question6 = Question(
-            "6",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            2)
+            "Selembar kertas berbentuk segitiga sama sisi memiliki keliling yang panjangnya 114 cm, maka panjang sisi-sisinya adalah",
+            "34cm",
+            "36cm",
+            "38cm",
+            3)
         quizList2.add(question6)
 
         val question7 = Question(
-            "7",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            2)
+            "Di bawah ini yang bukan termasuk ciri-ciri persegi panjang adalah ....",
+            "Keempat sisinya sama panjang",
+            "Mempunyai 2 simetri lipat",
+            "Keempat sudutnya siku-siku",
+            1)
 
         quizList2.add(question7)
         val question8 = Question(
-            "8",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
+            "Sebuah kolam renang berbentuk persegi panjang memiliki panjang 40 meter dan lebar 20 meter. Kolam renang tersebut dikelilingi jalan setapak selebar 1 meter. Luas jalan setapak itu adalah .... m²",
+            "122m²",
+            "124m²",
+            "126m²",
             2)
         quizList2.add(question8)
 
         val question9 = Question(
-            "9",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            2)
+            "Sebuah taman berbentuk lingkaran, kelilingnya adalah 3.850 m. Diameter taman tersebut adalah .... m",
+            "1.225cm",
+            "1.220cm",
+            "1.230cm",
+            1)
         quizList2.add(question9)
 
         val question10 = Question(
-            "10",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            2)
+            "Ibu membuat taplak meja berbentuk lingkaran berdiameter 1,4 m. Setelah jadi, ibu mengukur keliling taplak meja tersebut dan ternyata panjangnya adalah .... meter.",
+            "1,14m",
+            "3,4m",
+            "4,4m",
+            3)
         quizList2.add(question10)
 
         quizList2.shuffle()
@@ -182,85 +183,85 @@ object Util {
     }
 
     fun setHardQuestion(): ArrayList<Question> {
-        val quizList3 = ArrayList<Question>()
         val question1 = Question(
-            "6",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
+            "Sebuah lapangan berbentuk persegi panjang berukuran 250 m x 200 m. Budi berlari mengelilingi lapangan tersebut 3 kali. Maka jarak yang ditempuh Budi adalah .... meter.",
+            "2.700m",
+            "2.750m",
+            "2.800m",
             1)
         quizList3.add(question1)
 
         val question2 = Question(
+            "Ayah membuat papan tulis berukuran 200 cm x 150 cm. Papan tulis tersebut membutuhkan 2 kaleng cat berukuran kecil untuk tiap m². Cat yang dibutuhkan ayah untuk mengecat papan tulis tersebut adalah .... kaleng.",
+            "4",
             "5",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            2)
+            "6",
+            3)
         quizList3.add(question2)
 
         val question3 = Question(
-            "3",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
+            "Pekarangan belakang rumah paman berbentuk persegi panjang berukuran panjang 68 meter dan lebar 45 meter akan dibuatkan pagar dari bambu. Tiap meter membutuhkan 3 bambu. Banyaknya bambu yang dibutuhkan untuk membuat pagar pekarangan adalah ....",
+            "670",
+            "678",
+            "675",
             2)
         quizList3.add(question3)
 
         val question4 = Question(
-            "4",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            2)
+            "Sebuah lantai berbentuk persegi dengan panjang sisinya 12 m. Lantai tersebut akan dipasang ubin berbentuk persegi berukuran 30 cm x 30 cm. Banyaknya ubin yang diperlukan untuk menutup lantai adalah .... ubin.",
+            "1.600",
+            "1.700",
+            "1.500",
+            1)
         quizList3.add(question4)
 
         val question5 = Question(
-            "5",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            2)
+            "Selembar kertas karton berbentuk persegi dengan panjang 56 cm. Luas dan keliling kertas karton tersebut adalah ....",
+            "3.136 cm² dan 224 cm",
+            "3.146 cm² dan 234 cm",
+            "3.156 cm² dan 244 cm",
+            1)
         quizList3.add(question5)
 
         val question6 = Question(
-            "6",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
+            "Sebuah pekarangan berbentuk persegi. Panjang sisi pekarangan 86 m. Di sekeliling pekarangan itu akan ditanami pohon pepaya dengan jarak antarpohon 2 m. Banyak pohon pepaya yang dibutuhkan adalah .... pohon",
+            "170",
+            "172",
+            "174",
             2)
         quizList3.add(question6)
 
         val question7 = Question(
-            "7",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            2)
+            "Sebuah taman berbentuk lingkaran dengan diameter 56 meter akan ditanami rumput. Harga rumput adalah RP 7.500,00/m². Biaya yang harus dikeluarkan untuk membeli rumput adalah ....",
+            "Rp 18.480.000,00",
+            "Rp 18.350.000,00",
+            "Rp 18.560.000,00",
+            1)
 
         quizList3.add(question7)
         val question8 = Question(
-            "8",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            2)
+            "Panjang seluruh lengkung lingkaran disebut....",
+            "Jari-jari",
+            "Diameter",
+            "Keliling Lingkaran",
+            3)
         quizList3.add(question8)
 
         val question9 = Question(
-            "9",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            2)
+            "Seorang anak berlari mengelilingi sebuah lapangan berbentuk segitiga sama sisi yang memiliki ukuran sisi 20 m sebanyak 5 kali putaran. Hitunglah berapa jarak yang ditempuh anak tersebut!",
+            "300",
+            "250",
+            "200",
+            1)
         quizList3.add(question9)
 
         val question10 = Question(
-            "13",
-            "35 cm² dan 27 cm",
-            "27 cm² dan 35 cm",
-            "30 cm² dan 25 cm",
-            2)
+            "Tentukanlah keliling segitiga dengan panjang sisi-sisi berikut:\n" +
+                    "   A = 5 cm, B = 7 cm, dan C = 9 cm.",
+            "21cm",
+            "18cm",
+            "14cm",
+            1)
         quizList3.add(question10)
 
         quizList3.shuffle()
